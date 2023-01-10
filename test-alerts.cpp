76d36374972e7d5943e74adccef0c6c75909fce2
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2023-01-10 22:49:41 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,8 +1,13 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
+#define CATCH_CONFIG_MAIN
+#include <unistd.h>
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(20, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(30, 20, 30) == NORMAL);
+  REQUIRE(inferBreach(-10.0, 100.2, 1000.67) == TOO_LOW);
 }
